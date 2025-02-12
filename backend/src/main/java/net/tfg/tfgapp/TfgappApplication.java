00b09ae875a65944b10faeaa7d43df8092515e01
robberm <roberto.marcos@alumnos.upm.es>
2025-02-12 12:14:17 +0100

    Rob NTT: Project Setup

---
+++ backend/src/main/java/net/tfg/tfgapp/TfgappApplication.java
@@ -0,0 +1,13 @@
+package net.tfg.tfgapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/net/tfg/tfgapp/TfgappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tfg.tfgapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
