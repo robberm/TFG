a69d59eb643dd5b7afc7b4b761d8f0f4db07d235
robberm <roberto.marcos@alumnos.upm.es>
2026-06-23 20:16:05 +0200

    fixes

--- backend/src/main/java/net/tfg/tfgapp/domains/Objective.java
+++ backend/src/main/java/net/tfg/tfgapp/domains/Objective.java
@@ -2,6 +2,7 @@ package net.tfg.tfgapp.domains;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
